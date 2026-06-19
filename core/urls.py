80f93e2b756a3383688f86eca320971fa991a81b
Aminnaghieh <123194253+Aminnaghieh@users.noreply.github.com>
2026-06-19 12:02:42 +0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,9 @@
-﻿
+﻿from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('dashboard/', views.get_dashboard_data, name='dashboard'),
+    path('note/add/', views.add_note, name='add_note'),
+    path('vibe/update/', views.update_vibe, name='update_vibe'),
+    path('game/score/', views.submit_game_score, name='game_score'),
+]
