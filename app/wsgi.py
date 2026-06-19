878c1ea871c801a1664a3878753f83ec18a78d13
Aminnaghieh <123194253+Aminnaghieh@users.noreply.github.com>
2026-06-19 14:20:24 +0330

    Delete app directory

--- app/wsgi.py
+++
@@ -1,7 +0,0 @@
-﻿import os
-from django.core.wsgi import get_wsgi_application
-
-# توجه: چون پوشه شما اسمش app هست، اینجا هم app.settings نوشته شده
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
-application = get_wsgi_application()
