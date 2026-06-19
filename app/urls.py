613e78ed5e46caff3e3e75a0ca3210bf74025430
Aminnaghieh <123194253+Aminnaghieh@users.noreply.github.com>
2026-06-19 11:57:08 +0330

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,13 @@
-﻿
+﻿from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # تمام درخواست‌های api/ به فایل core/urls.py فرستاده میشن
+    path('api/', include('core.urls')),
+    
+    # صفحه اصلی مینی‌اپ (index.html) رو سرو می‌کنه
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+]
